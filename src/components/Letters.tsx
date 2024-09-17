3b04bc52a71855a5d63ab03ef41c695182d6f6b3
David Lipson <dlipson96@gmail.com>
2024-09-17 18:19:28 -0400

    animation

--- src/components/Letters.tsx
+++ src/components/Letters.tsx
@@ -1,23 +1,71 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { GameContext } from "../App";
-import { Stack, Typography } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
 import { styled } from "@mui/material/styles";
+import { LETTERS } from "../constants/constants";
 
 const LetterContainer = styled(Stack)(({ theme }) => ({
   flexDirection: "row",
   gap: "128px",
   [theme.breakpoints.down("sm")]: {
-    gap: "64px",
+    gap: "32px",
   },
 }));
 
+const getRandomLetters = (count = 10) => {
+  const shuffled = LETTERS.sort(() => 0.5 - Math.random());
+  return shuffled.slice(0, count);
+};
+
 // add animation! jumbling letters
 export const Letters = () => {
+  const [animate, setAnimate] = useState(true);
   const { letters } = useContext(GameContext);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAnimate(false);
+    }, 2000);
+    return () => clearTimeout(timeout);
+  }, []);
+
   return (
     <LetterContainer>
-      {letters.map((letter) => (
-        <Typography variant="h1">{letter.toUpperCase()}</Typography>
+      {letters.map((letter, index) => (
+        <Box overflow="hidden" height="120px" width="75px">
+          <Stack
+            sx={{
+              animation: animate
+                ? `slotMachine ${
+                    1 + index / 2
+                  }s cubic-bezier(0.25, 0.1, 0.25, 1)`
+                : "none",
+              "@keyframes slotMachine": {
+                "0%": {
+                  transform: "translateY(0)",
+                },
+                "100%": {
+                  transform: "translateY(-1200px)",
+                },
+              },
+              animationIterationCount: 1,
+              animationFillMode: "forwards",
+            }}
+            alignItems="center"
+            justifyContent="center"
+            direction="column"
+          >
+            {animate &&
+              getRandomLetters().map((letter) => (
+                <Typography height="120px" width="75px" variant="h1">
+                  {letter.toUpperCase()}
+                </Typography>
+              ))}
+            <Typography height="120px" width="75px" variant="h1">
+              {letter.toUpperCase()}
+            </Typography>
+          </Stack>
+        </Box>
       ))}
     </LetterContainer>
   );
